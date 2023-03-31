24b84b8a9b62e653663e9d72c1a963a3bf63be9f
Casti28 <lorenzo.castiello04@gmail.com>
2023-03-31 16:05:18 +0200

    esercizio 1

---
+++ EducazioneCivicaTests.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using System;
+using EducazioneCivica;
+
+public class EducazioneCivicaTests
+{
+    [Fact]
+    public void Esercizio1Test() 
+    { 
+        var test = new EducazioneCivica(); 
+        string result = test.Esercizio1("ciao", 10);
+
+        Assert.Equal(result, "abcd");
+    }
+}
